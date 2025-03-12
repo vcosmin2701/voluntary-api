828df28dd59af23e0aa9c458bf57a3a20058bb2d
vcosmin2701 <v.cosmin2717@gmail.com>
2025-03-12 23:01:39 +0200

    Add validation to the Volunteer Model

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,7 @@
 class Volunteer < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :date_joined, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 15, allow_nil: true }
 end
