9c3a26cb49b0190549c3257fac6c6731123a0725
vcosmin2701 <v.cosmin2717@gmail.com>
2025-03-12 22:57:16 +0200

    Add simple validation and contraints + migration

--- db/migrate/20250312205026_create_volunteers.rb
+++ db/migrate/20250312205026_create_volunteers.rb
@@ -1,15 +1,16 @@
 class CreateVolunteers < ActiveRecord::Migration[8.0]
   def change
     create_table :volunteers do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.integer :age
-      t.string :email
+      t.string :email, null: false
       t.string :role
       t.string :phone
-      t.date :date_joined
+      t.date :date_joined, null: false
 
       t.timestamps
     end
+    add_index :volunteers, :email, unique: true # ensure that the values from the email column are unique
   end
 end
