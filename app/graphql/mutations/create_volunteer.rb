95b24e4cc117eb2dd5b4215af22835f6ce7617b3
vcosmin2701 <v.cosmin2717@gmail.com>
2025-03-13 10:47:45 +0200

    GraphQL Mutation for adding a volunteer

---
+++ app/graphql/mutations/create_volunteer.rb
@@ -0,0 +1,34 @@
+module Mutations
+  class CreateVolunteer < BaseMutation
+    argument :attributes, Types::VolunteerInputType, required: true
+
+    field :volunteer, Types::VolunteerType, null: true
+    field :errors, [String], null: false
+
+    def resolve(attributes:)
+      formatted_date_joined = attributes[:date_joined]
+
+      volunteer = Volunteer.new(
+        first_name: attributes[:first_name],
+        last_name: attributes[:last_name],
+        age: attributes[:age],
+        email: attributes[:email],
+        role: attributes[:role],
+        phone: attributes[:phone],
+        date_joined: formatted_date_joined,
+      )
+
+      if volunteer.save
+        {
+          volunteer: volunteer,
+          errors: [],
+        }
+      else
+        {
+          volunteer: nil,
+          errors: volunteer.errors.full_messages
+        }
+      end
+    end
+  end
+end
