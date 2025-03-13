96838ce41bc396ee070a712956aa9dad84ce2811
vcosmin2701 <v.cosmin2717@gmail.com>
2025-03-13 22:41:29 +0200

    Register GraphQL mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,8 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    field :create_volunteer, mutation: Mutations::CreateVolunteer
+    field :update_volunteer, mutation: Mutations::UpdateVolunteer
+    field :delete_volunteer, mutation: Mutations::DeleteVolunteer
   end
 end
